201462378563349aec0e16230fbdc37023734898
JBH-0 <bohyun929@naver.com>
2022-10-30 22:12:57 +0900

    검색 기능 구현하기

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.PostList.as_view()),
     path('create_post/', views.PostCreate.as_view()),
     path('update_post/<int:pk>/', views.PostUpdate.as_view()),
+    path('search/<str:q>/', views.PostSearch.as_view()),
     #path('<int:pk>/', views.single_post_page),
     #path('', views.index),
 
