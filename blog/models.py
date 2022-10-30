1c40a179321cef212e0f424665e2c14a18e6fca8
JBH-0 <bohyun929@naver.com>
2022-10-30 18:21:36 +0900

    Content 입력란에 마크다운 적용

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from markdownx.models import MarkdownxField
+from markdownx.utils import markdown
 import os
 
 class Post(models.Model):
     title = models.CharField(max_length=30) #제목
-    content = models.TextField() #내용
+    content = MarkdownxField() #내용
 
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
     file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
@@ -20,3 +22,6 @@ class Post(models.Model):
 
     def get_file_name(self):
         return os.path.basename(self.file_upload.name)
+
+    def get_content_markdown(self):
+        return markdown(self.content)
