8d8e5025a27cef230fc612adf6b0b227a47f3f33
JBH-0 <bohyun929@naver.com>
2022-10-30 17:30:22 +0900

    포스트 작성

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
-from django.views.generic import ListView, DetailView
+from django.shortcuts import redirect
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class PostList(ListView):
     model = Post
@@ -7,3 +9,15 @@ class PostList(ListView):
 
 class PostDetail(DetailView):
     model = Post
+
+class PostCreate(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content', 'head_image', 'file_upload']
+
+    def form_valid(self, form):
+        current_user = self.request.user
+        if current_user.is_authenticated:
+            form.instance.author = current_user
+            return super(PostCreate, self).form_valid(form)
+        else:
+            return redirect('/blog/')
