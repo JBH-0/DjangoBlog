978fba12e66a068e6b392e1f8932c777353e1a71
JBH-0 <bohyun929@naver.com>
2022-11-01 14:50:34 +0900

    사용자 아바타 보여주기

--- blog/models.py
+++ blog/models.py
@@ -37,3 +37,9 @@ class Comment(models.Model):
 
     def get_absolute_url(self):
         return f'{self.post.get_absolute_url()}#comment-{self.pk}'
+    
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else:
+            return f'https://doitdjango.com/avatar/id/1324/2996c7f3f246b634/svg/{ self.author.email }'
